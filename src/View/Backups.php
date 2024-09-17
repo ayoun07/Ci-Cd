77595e544091fb39476e50a67eef2c6d44e5ba9f
ayoun07 <ayoun07>
2024-09-17 10:08:44 +0200

    retour de la navbar

--- src/View/Backups.php
+++ src/View/Backups.php
@@ -6,15 +6,19 @@ require "header.php";
 <html>
 
 <body>
+    <script src="/public/js/database.js" type="module"></script>
     <div class="container-fluid d-flex">
         <?php require_once("NavBar.php") ?>
         <div class="d-flex flex-column w-100">
             <!-- Container pour le titre et le bouton -->
             <div class="d-flex justify-content-between mb-3">
-                <h1 class="titre">Backups</h1>
+                <h1 class="titre">Databases</h1>
                 <!-- Bouton placé à l'extrême droite et légèrement en bas du titre -->
                 <button type="button" id="btnajout" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-primary w-10">
                     Create backup +
+                </button>
+                <button type="button" id="btnajout" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-secondary w-10">
+                    Create task +
                 </button>
             </div>
 
@@ -29,32 +33,26 @@ require "header.php";
                             <th scope="col">URL</th>
                             <th scope="col">Used_type</th>
                             <th scope="col">User-database</th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td>@fat</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                        </tr>
+                        <?php foreach ($databases as $database) { ?>
+
+
+                            <tr>
+                                <th scope="row"><?= $database['id'] ?></th>
+                                <td><?= $database['nom'] ?></td>
+                                <td><?= $database['port'] ?></td>
+                                <td><?= $database['url'] ?></td>
+                                <td><?= $database['used_type'] ?></td>
+                                <td><?= $database['user_database'] ?></td>
+                                <td><button class="btn btn-danger delete" type="button" id="<?= $database['id'] ?>">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
@@ -62,19 +60,19 @@ require "header.php";
             <!-- Modal d'ajout de base de données -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1>Creer une sauvegarde</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="/database/create" method="post">
+                    <form action="/database" method="post">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1>Création d'une tache planifier</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
                                 <div class="input-group mb-3">
                                     <label class="form-label" for="name">Nom database: </label>
                                     <input type="text" id="name" name="name" required>
                                 </div>
                                 <div class="input-group mb-3">
-                                    <label class="form-label" for="user">Nom d'utilisateur: </label>
+                                    <label class="form-label" for="user">Date de démarrage </label>
                                     <input type="text" id="user" name="user" required>
                                 </div>
                                 <div class="input-group mb-3">
@@ -96,13 +94,13 @@ require "header.php";
                                     <label class="form-label" for="host">URL: </label>
                                     <input type="text" id="host" name="host" required>
                                 </div>
-                            </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary" id="Valider">Save changes</button>
+                            </div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" type="submit" id="Valider">Save changes</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
