a831b04d31930afd5bae8f75610dce316f0db177
Lionelc13 <lionelc.13@gmail.com>
2024-09-17 09:37:35 +0200

    change backup v1

--- src/View/Backups.php
+++ src/View/Backups.php
@@ -7,7 +7,7 @@ require "header.php";
 
 <body>
     <div class="container-fluid d-flex">
-        <?php require_once("NavBar.php") ?>
+        <?php require_once "NavBar.php" ?>
         <div class="d-flex flex-column w-100">
             <!-- Container pour le titre et le bouton -->
             <div class="d-flex justify-content-between mb-3">
@@ -29,32 +29,26 @@ require "header.php";
                             <th scope="col">URL</th>
                             <th scope="col">Used_type</th>
                             <th scope="col">User-database</th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td>@fat</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                        </tr>
+                        <?php foreach ($backups as $backup) { ?>
+
+
+                            <tr>
+                                <th scope="row"><?= $backup['id'] ?></th>
+                                <td><?= $backup['database'] ?></td>
+                                <td><?= $backup['version'] ?></td>
+                                <td><?= $backup['url'] ?></td>
+                                <td><?= $backup['used_type'] ?></td>
+                                <td><?= $database['user_database'] ?></td>
+                                <td><button class="btn btn-danger delete" type="button" id="<?= $database['id'] ?>">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
@@ -68,33 +62,36 @@ require "header.php";
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="/database/create" method="post">
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="name">Nom database: </label>
-                                    <input type="text" id="name" name="name" required>
+                            <form action="/cron/create" method="post">
+                                <div>
+                                    <label for="nom">Nom de la tache: </label><input type="text" id="nom" name="nom" required>
+                                    <div>
+
+                                        <label for="iddatabase">Type de base de données :</label>
+                                        <select name="nom" id="type">
+                                            <?php foreach ($databases as $database) { ?>
+                                                <option value=" <?= $database['id'] ?>"><?= $database['nom'] ?></option>
+                                            <?php } ?>
+                                        </select>
+                                    </div>
+
+
+
                                 </div>
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="user">Nom d'utilisateur: </label>
-                                    <input type="text" id="user" name="user" required>
+                                <div>
+                                    <label for="iddatabase">Nom database: </label><input type="text" id="iddatabase" name="iddatabase" required>
                                 </div>
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="password">Mot de passe: </label>
-                                    <input type="password" id="password" name="password" required>
+                                <div>
+                                    <label for="datestart">Date de démarrage </label><input type="text" id="datestart" name="datestart" required>
                                 </div>
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="type">Type de base de données :</label>
-                                    <select name="type" id="type">
-                                        <option value="1">mysql</option>
-                                        <option value="2">pgsql</option>
-                                    </select>
+                                <div>
+                                    <label for="heure">Heure: </label><input type="text" id="heure" name="heure" required>
                                 </div>
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="port">Port: </label>
-                                    <input type="text" id="port" placeholder="default or port number" name="port" required>
+                                <div>
+                                    <label for="port">Port: </label><input type="text" id="port" name="port" required>
                                 </div>
-                                <div class="input-group mb-3">
-                                    <label class="form-label" for="host">URL: </label>
-                                    <input type="text" id="host" name="host" required>
+                                <div>
+                                    <button type="submit" id="Valider">Valider</button>
                                 </div>
                             </form>
                         </div>
