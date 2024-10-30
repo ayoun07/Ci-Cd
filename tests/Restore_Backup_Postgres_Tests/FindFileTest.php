3e584a4cc8198393916f9e99d58c3aa6fe61bd57
ayoun07 <ayoun07>
2024-10-30 16:05:10 +0100

    testv17

--- tests/Restore_Backup_Postgres_Tests/FindFileTest.php
+++ tests/Restore_Backup_Postgres_Tests/FindFileTest.php
@@ -6,13 +6,14 @@ use PHPUnit\Framework\TestCase;
 class FindFileTest extends TestCase{
     public function testfindfileexist(){
         $object=new FindFile();
-        $fileexist = $object->findFile('C:\wamp64\www\safebase\tests\Restore_Backup_Postgres_Tests\backup_postgres.sql');
+        //$fileexist = $object->findFile('C:\wamp64\www\safebase\tests\Restore_Backup_Postgres_Tests\backup_postgres.sql');
+        $fileexist = $object->findFile('../Restore_Backup_Postgres_Tests/backup_postgres.sql');
         $this->assertTrue($fileexist);
     }
     public function testfindfileexistFailures()
     {
         $object = new FindFile();
-        $fileexist = $object->findFile('C:\wamp64\www\safebasejyyjyrjyr\tests\Restore_Backup_Postgres_Tests\backup_postgress.sql');
+        $fileexist = $object->findFile('../Restore_Backup_Postgres_Tests/backup_postgres.sql');
         $this->assertFalse($fileexist);
     }
 }
