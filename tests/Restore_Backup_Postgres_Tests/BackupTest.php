6ec0e59b30b5d4dc2bb8d4412e4ae7e9e38fef30
ayoun07 <ayoun07>
2024-10-30 16:20:03 +0100

    testv18

--- tests/Restore_Backup_Postgres_Tests/BackupTest.php
+++ tests/Restore_Backup_Postgres_Tests/BackupTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use SAFEBASE\Restore_Backup_Postgres\Backup;
-use PHPUnit\Framework\TestCase;
+// use SAFEBASE\Restore_Backup_Postgres\Backup;
+// use PHPUnit\Framework\TestCase;
 
-class BackupTest extends TestCase{
+// class BackupTest extends TestCase{
 
 
-    public function testbackup():void{
-        $backup = new Backup('basepostgres', 'localhost', 'Postmalone0751@', 'postgres', 5432);
-        $this->assertTrue( $backup->backup('backup_postgres.sql'));
-    }
-}   
+//     public function testbackup():void{
+//         $backup = new Backup('basepostgres', 'localhost', 'Postmalone0751@', 'postgres', 5432);
+//         $this->assertTrue( $backup->backup('backup_postgres.sql'));
+//     }
+// }   
--- tests/Restore_Backup_Postgres_Tests/FindFileTest.php
+++ tests/Restore_Backup_Postgres_Tests/FindFileTest.php
@@ -7,13 +7,13 @@ class FindFileTest extends TestCase{
     public function testfindfileexist(){
         $object=new FindFile();
         //$fileexist = $object->findFile('C:\wamp64\www\safebase\tests\Restore_Backup_Postgres_Tests\backup_postgres.sql');
-        $fileexist = $object->findFile('../Restore_Backup_Postgres_Tests/backup_postgres.sql');
+        $fileexist = $object->findFile(__DIR__.'/backup_postgres.sql');
         $this->assertTrue($fileexist);
     }
     public function testfindfileexistFailures()
     {
         $object = new FindFile();
-        $fileexist = $object->findFile('../Restore_Backup_Postgres_Tests/backup_postgres.sql');
+        $fileexist = $object->findFile(__DIR__.'/backup_postgres.sql');
         $this->assertFalse($fileexist);
     }
 }
