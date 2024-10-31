a7e5d70613f93295d68355f81d66aad99dfa39b2
ayoun07 <ayoun07>
2024-10-31 10:21:45 +0100

    testv29

---
+++ tests/Restore_Backup_Postgres_Tests/BackupTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use SAFEBASE\Restore_Backup_Postgres\Backup;
+use PHPUnit\Framework\TestCase;
+
+class BackupTest extends TestCase
+{
+
+
+    public function testbackup(): void
+    {
+        $backup = new Backup('basepostgres', 'localhost', 'Postmalone0751@', 'postgres', 5432);
+        $this->assertTrue($backup->backup('backup_postgres.sql'));
+    }
+}
